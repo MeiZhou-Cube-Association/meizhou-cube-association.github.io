00b541d481f6e2945ee4a8e1cfea5c63e65bab84
Alex_Beng <599267065@qq.com>
2019-07-23 17:29:07 +0800

    add log

--- DateGetter/DataBase.py
+++ DateGetter/DataBase.py
@@ -1,12 +1,12 @@
 def AddId(info, db_path):
     file = open(db_path+'wca_id.csv', encoding='utf-8', mode='r')
+    print(info)
+    info = [info[1][:10], info[0]]
     names=file.readlines()
     for i in names:
-        if i[:10] == info[1]:
+        if i[:10] == info[0]:
             return
     file = open(db_path+'wca_id.csv', encoding='utf-8', mode='a')
-    # print(names)
-    del info[-1]
-    info[0], info[1] = info[1], info[0]
+
     file.write(','.join(info)+'\n')
     file.close()

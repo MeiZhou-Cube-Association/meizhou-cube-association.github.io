7f2d31429ecddd71b0f0436e3eda74b988b20235
Alex_Beng <599267065@qq.com>
2019-07-29 21:55:47 +0800

    mzr v1.0

--- DateGetter/Spider.py
+++ DateGetter/Spider.py
@@ -1,11 +1,14 @@
+import time
 import requests
 from bs4 import BeautifulSoup as BS
 
 #To get HTML Text
 def GetHtml(url):
-    kv = {'user-agent':'Mozilla/5.0'}
+    kv = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',        
+    }
     try:
-        r = requests.get(url, timeout=30,headers = kv)
+        r = requests.get(url, timeout=7,headers = kv)
         r.raise_for_status()
         r.encoding=r.apparent_encoding
         return r.text
@@ -69,8 +72,12 @@ def GetPerform(wca_id):
     people_soup = BS(people_html, 'html.parser')
 
     event2perform = {}
-    
-    event_list = people_soup.tbody.find_all('tr')
+    try:
+        event_list = people_soup.tbody.find_all('tr')
+    except:
+        print("鸭鸭鸭")
+        time.sleep(23)
+        return GetPerform(wca_id)
     for event in event_list:
         # 因为不一定就是event的表格
         try:
@@ -84,16 +91,19 @@ def GetPerform(wca_id):
                 )
         except:
             continue
-    event2perform['comp_times'] = people_soup.find_all('span')[6].string
-    event2perform['roa_avg'] = people_soup.find_all('tbody')[1].find_all('tr')[1].find_all('td')[1].string
-    event2perform['roa_sig'] = people_soup.find_all('tbody')[1].find_all('tr')[0].find_all('td')[1].string
+    event2perform['roa'] = (
+        people_soup.find_all('tbody')[1].find_all('tr')[0].find_all('td')[1].string,
+        people_soup.find_all('tbody')[1].find_all('tr')[1].find_all('td')[1].string
+    )
 
     solved_trs = people_soup.find_all('tbody')[0].find_all('tr')
     solved_times = 0
     for tr in solved_trs:
         solved_times += int(tr.find_all('td')[-1].string.split('/')[0])
 
-    event2perform['solve_times'] = str(solved_times)
+    event2perform['comp_solve'] = (
+        people_soup.find_all('span')[6].string, str(solved_times)
+    )
     return event2perform
 
 def GenMessage(name):
